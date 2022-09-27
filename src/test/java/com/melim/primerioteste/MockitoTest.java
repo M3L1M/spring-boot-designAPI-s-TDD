903ab822abbfa8509a64ffa2957866d546657613
M3L1M <45291805+M3L1M@users.noreply.github.com>
2022-09-27 17:58:58 -0300

    Commit da seção 2

--- src/test/java/com/melim/primerioteste/MockitoTest.java
+++ src/test/java/com/melim/primerioteste/MockitoTest.java
@@ -1,23 +1,39 @@
 package com.melim.primerioteste;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTest {
+	
+	@Mock
+	List<String> lista;
+	
 	
 	@Test 
 	public void primeiroTesteMockito() {
-		List<String> lista=Mockito.mock(ArrayList.class);
+		/*Mockito.when(lista.size()).thenReturn(2);
+		int size = 0;
+		if(1==1) {
+			size = lista.size();
+			size = lista.size();
+		} */
 		
-		Mockito.when(lista.size()).thenReturn(2);
+		lista.size();
+		lista.size();
 		
-		int size = lista.size();
+		InOrder inOrder = Mockito.inOrder(lista);
+		inOrder.verify(lista).size();
+		inOrder.verify(lista).add("");
 		
-		Assertions.assertThat(size).isEqualTo(20);
+		//Assertions.assertThat(size).isEqualTo(2);
 		
+		//Mockito.verify(lista,Mockito.times(1)).size();
 	}
 }
